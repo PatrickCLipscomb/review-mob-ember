2b9b86a456a0ae0a1462899f1c1be888511a74ef
Patrick Lipscomb <patrick@>
2016-08-26 12:16:36 -0700

    add the ability to add ratings to restaurants

---
+++ app/components/rest-reviewer.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  choice: null,
+  actions: {
+    scoreToggled(choice) {
+      this.set('choice', choice);
+    },
+    createRating(restaurant) {
+      var params = {
+        note: this.get('note'),
+        author: this.get('author'),
+        score: parseInt(this.get('choice')),
+        restaurant: this.get('model'),
+      };
+      this.sendAction('createRating', params);
+      this.set('giveRating', false)
+    }
+  }
+});
